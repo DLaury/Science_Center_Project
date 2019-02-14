b0e50dda360efa20f6c49a7e2339372a496d504b
Maria Jadoon <41587785+Mariajadoon@users.noreply.github.com>
2019-02-13 19:44:38 -0600

    web changes

--- Project2/app.py
+++ Project2/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/index3/')
+def index3():
+    return render_template('index3.html')
+
 if __name__ == "__main__":
     app.run()
