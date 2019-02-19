9d17fbd5aa04004509f3ccdcd84b33b1d3c9aef7
Tchjdaedn <43163522+tchjdaedn@users.noreply.github.com>
2019-02-18 19:30:08 -0600

    added index 2

--- Project2/app.py
+++ Project2/app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/index2/')
+def index2():
+    return render_template('index2.html')
+
 @app.route('/index3/')
 def index3():
     return render_template('index3.html')
